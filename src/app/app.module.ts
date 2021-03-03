c0d7b1437517a05ab15b4a4739425e18fd3003ca
Fernando Piñeira <fptarghetta@gmail.com>
2021-03-04 00:20:01 +0100

    formulario bienvenida metidoç

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { FormBienvenidaComponent } from './components/shared/form-bienvenida/form-bienvenida.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    FormBienvenidaComponent
   ],
   imports: [
     BrowserModule
